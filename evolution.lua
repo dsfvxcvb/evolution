66868a8d994c8a06001e276bb40d45d7fc13bcf6
Kimi <kimi@local>
2026-07-05 22:49:23 -0700

    Loader: fallback to hoodcustoms for unsupported games

--- evolution.lua
+++ evolution.lua
@@ -5,5 +5,5 @@ if game.PlaceId == 9825515356 then
 elseif game.PlaceId == 90568084448279 then
     loadstring(game:HttpGet(repo .. "evolution_onetap.lua"))()
 else
-    game:GetService("Players").LocalPlayer:Kick("[evolution] Game not supported (PlaceId " .. tostring(game.PlaceId) .. ")")
+    loadstring(game:HttpGet(repo .. "evolution_hood.lua"))()
 end
