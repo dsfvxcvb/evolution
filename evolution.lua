ae45876b2a2680394084d8b230df1b06635ceadc
Kimi <kimi@local>
2026-07-05 20:09:39 -0700

    Update loader to load modules from GitHub raw URLs

--- evolution.lua
+++ evolution.lua
@@ -1,7 +1,9 @@
+local repo = "https://raw.githubusercontent.com/dsfvxcvb/evolution/main/"
+
 if game.PlaceId == 9825515356 then
-    loadfile("C:/Users/Adkin/Downloads/scripts/evolution_hood.lua")()
+    loadstring(game:HttpGet(repo .. "evolution_hood.lua"))()
 elseif game.PlaceId == 90568084448279 then
-    loadfile("C:/Users/Adkin/Downloads/scripts/evolution_onetap.lua")()
+    loadstring(game:HttpGet(repo .. "evolution_onetap.lua"))()
 else
     game:GetService("Players").LocalPlayer:Kick("[evolution] Game not supported (PlaceId " .. tostring(game.PlaceId) .. ")")
 end
