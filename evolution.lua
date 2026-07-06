d6dba6cb2f1e7eebe0a55339e286680ae21a4423
Kimi <kimi@local>
2026-07-05 23:06:03 -0700

    Loader: load protected.lua before hoodcustoms module

--- evolution.lua
+++ evolution.lua
@@ -1,9 +1,11 @@
 local repo = "https://raw.githubusercontent.com/dsfvxcvb/evolution/main/"
 
 if game.PlaceId == 9825515356 then
+    loadstring(game:HttpGet(repo .. "protected.lua"))()
     loadstring(game:HttpGet(repo .. "evolution_hood.lua"))()
 elseif game.PlaceId == 90568084448279 then
     loadstring(game:HttpGet(repo .. "evolution_onetap.lua"))()
 else
+    loadstring(game:HttpGet(repo .. "protected.lua"))()
     loadstring(game:HttpGet(repo .. "evolution_hood.lua"))()
 end
